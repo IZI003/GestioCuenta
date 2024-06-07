bceffc76f7972c37b10ee25b3bfa17aee765333f
Ezequiel.Miranda <mirandaezquiel@gmail.com>
2024-06-07 00:18:12 -0300

    Cuenta y Operaciones

---
+++ Controllers/CuentaController.cs
@@ -0,0 +1,86 @@
+﻿using Comunes.Config;
+using Comunes.Respuesta;
+using Cuenta.Modelos;
+using Cuenta.Servicios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cuenta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CuentaController : ControllerBase
+    {
+        private readonly ICuenta CuentaServices;
+
+        public CuentaController(ICuenta CuentaServices)
+        { 
+            this.CuentaServices = CuentaServices;
+        }
+        // GET: api/<CuentaController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CuentaController>/saldo/5
+        [HttpGet("saldo/{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var response = new RespuestaApi<SalidaSaldo>();
+            var salida = await  CuentaServices.ObtenerSaldoIdCuenta(id);
+
+            if (!salida.RespuestaBD.Ok)
+            {
+                response.Resultado.AgregarError(GestionErrores.C_Men_100, 403, mensaje: GestionErrores.C_Cod_100, codigoInterno: GestionErrores.C_Cod_100);
+
+                return response.ObtenerResult();
+            }
+            response.Resultado.Datos = salida;
+            return Ok(response);
+        }
+
+        [HttpGet("saldo-total/{idusuario}")]
+        public async Task<IActionResult> GetSaldoUsuario(int idusuario)
+        {
+            var response = new RespuestaApi<SalidaSaldoTotal>();
+            var salida = await CuentaServices.ObtenerSaldoIdUsuario(idusuario);
+
+            if (!salida.RespuestaBD.Ok)
+            {
+                response.Resultado.AgregarError(GestionErrores.C_Men_100, 403, mensaje: GestionErrores.C_Cod_100, codigoInterno: GestionErrores.C_Cod_100);
+
+                return response.ObtenerResult();
+            }
+            response.Resultado.Datos = salida;
+            return Ok(response);
+        }
+        // POST api/<CuentaController>
+        [HttpPost]
+        public async Task<IActionResult> CrearCuenta([FromBody] int idusuario)
+        {
+            var response = new RespuestaApi<RespuestaBD>();
+
+            var salida = await CuentaServices.CrearCuenta(idusuario);
+
+            if (!salida.Ok)
+            {
+                response.Resultado.AgregarError(GestionErrores.GC_Men_101, 400, mensaje: salida.Mensaje, codigoInterno: GestionErrores.GC_Cod_101);
+
+                return response.ObtenerResult();
+            }
+
+            response.Resultado.Datos = salida;
+
+            return Ok(response);
+        }
+
+        // PUT api/<CuentaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+    }
+}
---
+++ Servicios/Cuenta.cs
@@ -0,0 +1,76 @@
+﻿using AccesoDatos;
+using Comunes.Respuesta;
+using Dapper;
+using Cuenta.Modelos;
+
+namespace Cuenta.Servicios;
+
+public class Cuentas : ICuenta
+{
+    private BD bd;
+    public Cuentas(BD bd)
+    {
+        this.bd = bd;
+    }
+
+    public async Task<SalidaSaldo> ObtenerSaldoIdCuenta(int idCuenta)
+    {
+        var salida = new SalidaSaldo();
+        try
+        {
+            var query = @"select saldo,id_cuenta as cuenta 
+                        
+                        from cuenta where id_cuenta = @idCuenta";
+
+            using var con = bd.ObtenerConexion();
+            var datosDb = await con.QueryAsync<Saldo>(query, new { idCuenta });
+
+            salida.saldo = datosDb.FirstOrDefault();
+        }
+        catch (Exception ex)
+        {
+            salida.RespuestaBD = new RespuestaBD($"ERROR|Error al consultar los datos. {ex.Message}");
+        }
+
+        salida.RespuestaBD = new RespuestaBD("OK");
+        return salida;
+    }
+
+    public async Task<SalidaSaldoTotal> ObtenerSaldoIdUsuario(int idUsuario)
+    {
+        var salida = new SalidaSaldoTotal();
+        try
+        {
+            var query = "select SUM(saldo) as saldo from cuenta where id_usuario = @idUsuario";
+
+            using var con = bd.ObtenerConexion();
+            var datosDb = await con.QueryAsync<decimal>(query, new { idUsuario });
+
+            salida.saldo = datosDb.FirstOrDefault();
+        }
+        catch (Exception ex)
+        {
+            salida.RespuestaBD = new RespuestaBD($"ERROR|Error al consultar los datos. {ex.Message}");
+        }
+
+        salida.RespuestaBD = new RespuestaBD("OK");
+        return salida;
+    }
+
+    public async Task<RespuestaBD> CrearCuenta(int idUsuario)
+    {
+        var salida = new RespuestaBD();
+        try
+        {
+            using var con = bd.ObtenerConexion();
+            await con.QueryAsync($"Insert into cuenta (saldo,id_usuario) values (0,{idUsuario})");
+            salida = new RespuestaBD("OK");
+        }
+        catch (Exception ex)
+        {
+            salida = new RespuestaBD($"ERROR|Error al Crear la cuenta. {ex.Message}");
+        }
+
+        return salida;
+    }
+}
